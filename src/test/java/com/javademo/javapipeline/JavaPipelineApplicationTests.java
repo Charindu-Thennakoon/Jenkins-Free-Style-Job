c2dc6d332a72bb9a3137afa0addd35549d16e22a
charindu sadeep <charsad500@gmail.com>
2022-12-25 19:07:48 +0530

    add the spring boot hello-world REST with test

---
+++ src/test/java/com/javademo/javapipeline/JavaPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javademo.javapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
